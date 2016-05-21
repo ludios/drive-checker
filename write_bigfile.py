89af83763ffdfe89575516039f12a32474ff0a92
Ivan Kozik <ivan@ludios.org>
2016-05-21 18:12:40 +0000

    Use openssl in subprocess to get random data much faster

--- write_bigfile.py
+++ write_bigfile.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 
 from hashlib import md5
-import ssl
+import subprocess
 import sys
 
 fname = sys.argv[1]
 f = open(fname, "wb")
+# Both os.urandom and ssl.RAND_pseudo_bytes are slow, so use this monstrosity instead
+p = subprocess.Popen('openssl enc -aes-128-ctr -pass "pass:$(dd if=/dev/urandom bs=128 count=1 2>/dev/null | base64)" < /dev/zero', shell=True, stdout=subprocess.PIPE)
 while True:
-	rand = ssl.RAND_pseudo_bytes(256 * 1024)[0]
+	rand = p.stdout.read(256 * 1024)
 	dig = md5(rand).digest()
 	f.write(rand)
 	f.write(dig)
